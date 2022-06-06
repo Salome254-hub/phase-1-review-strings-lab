4bf15cd0196943a848c6e61c3e78fc0004a3494d
salomemabonga <salome.mabonga@student.moringaschool.com>
2022-06-06 21:23:04 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser='Gracey';
+
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!` ;
+
+const excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser.toUpperCase()} !`;
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+const shortGreeting=`Welcome, ${firstInitial} ${currentUser.slice(0,1)};
+}!`; 
+
+ 
